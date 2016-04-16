5cc257fd91040c7e273bede787237737db6290dd
Alexander Krantz <AlexanderKrantz@alexanders-macbook-pro.local>
2016-04-16 19:59:36 -0400

    Initial Commit

---
+++ combo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  combo
+//
+//  Created by Collin McQueen on 4/16/16.
+//  Copyright © 2016 mckrantz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Simple Commit")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
