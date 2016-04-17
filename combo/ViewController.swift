65cdd1daa928817f3bd8d776c0aa4879da0cbba2
Alexander Krantz <AlexanderKrantz@alexanders-macbook-pro.local>
2016-04-16 20:01:23 -0400

    Commit 4?

--- combo/ViewController.swift
+++ combo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Simple Commit")
+        print("Work!")
     }
 
     override func didReceiveMemoryWarning() {
